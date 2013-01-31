099713177dba56b772c0c7550e11427ea4efe64f
Toni Dahl <toni.dahl@cie.fi>
2013-01-31 10:02:55 +0200

    Added a shell script for converting .mesh files to collada automatically with OgreMeshToXML and assimp.

---
+++ tools/OgreMeshToDae.sh
@@ -0,0 +1,44 @@
+for i in *.mesh
+do
+    # Conversion from MESH to XML
+    echo "Converting $i to XML..."
+    OgreXMLConverter -q $i
+    xmlFile="$i.xml"
+
+    if [ ! -f $xmlFile ]
+    then
+        echo "MESH->XML conversion failed!"
+        continue
+    fi
+
+    echo "Done."
+
+    # Conversion from XML to COLLADA
+    echo "Converting $xmlFile to COLLADA..."
+
+    daeFile="${xmlFile/.mesh.xml}"
+    assimp export $xmlFile "$daeFile.temp.dae"
+
+    if [ ! -f "$daeFile.temp.dae" ]
+    then
+        echo "XML->COLLADA conversion failed!"
+        continue
+    fi
+
+    echo "Done."
+    rm -f $xmlFile
+
+    # Fixing the COLLADA file so textures display correctly
+    echo "Fixing stuff (that assimp broke) in $daeFile.temp.dae..."
+    sed '/TEXCOORD.*tex1/d' ./"$daeFile.temp.dae" > ./"$daeFile.dae"
+
+    if [ ! -f "$daeFile.dae" ]
+    then
+        echo "Fixing hack failed!"
+        continue
+    fi
+
+    rm -f "$daeFile.temp.dae"
+    echo "Done."
+
+done

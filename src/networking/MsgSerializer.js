77a329fb240acc9a38ada594d6e9cb3181645faa
Toni Dahl <toni.dahl@cie.fi>
2013-06-04 15:33:58 +0300

    Planning msg serializer.

--- src/networking/MsgSerializer.js
+++ src/networking/MsgSerializer.js
@@ -11,13 +11,61 @@
 
     util = namespace.util;
 
-    MsgSerializer = namespace.MsgSerializer = function ( framework ) {
+    MsgSerializer = namespace.MsgSerializer = function ( framework, type ) {
 
-        //TODO: Add methods for serializing data to messages
-
-        return {
-
+        var types = {
+            json: {parser: parseJson, serializer: serializeToJson},
+            knet: {parser: parseKNetData, serializer: serializeToKNet}
         };
+
+        if(!types.hasOwnProperty(type)){
+            throw Error(["MsgSerializer:", "Message type:", type, "not supported"]);
+        }
+
+        function parseMessage( data ){
+            return types[type].parser(data);
+
+        }
+
+        function serializeData ( event, data ){
+            return types[type].serializer(event, data);
+        }
+
+
+        function parseJson( data ){
+            data = JSON.parse( data );
+
+            if(!data['event'] || data['data']){
+                return false;
+            }
+
+            return data;
+        }
+
+        function serializeToJson( event, data ){
+
+            var entityId = data['entId'];
+
+
+            return '{"event":' + event+ ', "data":' +
+                '{"entityId":'+this.parent.id+''+
+                ',"attrs":{'+
+                '"'+this.id+'":{'+
+                '"0":'+JSON.stringify(this.placeable.transform)+
+                '}'+
+                '}'+
+                '}'+
+            '}';
+
+        }
+
+        function parseKNetData ( data ){
+
+        }
+
+        function serializeToKNet ( data ){
+
+        }
 
     };
 
